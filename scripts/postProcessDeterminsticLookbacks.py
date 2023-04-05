0c80a8d01307328b4944f16093ae8ca3f2f50ba6
Daniel Hamill <daniel.d.hamill@usace.army.mil>
2023-04-05 16:32:00 -0700

    notes to start lookback script

---
+++ scripts/postProcessDeterminsticLookbacks.py
@@ -0,0 +1,11 @@
+'''
+new model to use : "\\spk-netapp2\Hydrology\Studies\SAC-013\Folsom Dam Raise_SOU\99-Working Files\Hamill\Folsom Model\J6R7HW_SOU_Hindcast_2023.04.05.7z"
+- simulation and alternative names changed will need to update all processing
+
+THing to code to: SF 260 year
+- dss file: "\\spk-netapp2\Hydrology\Studies\SAC-013\Folsom Dam Raise_SOU\99-Working Files\Hamill\Folsom Model\1986E\simulation.dss"
+
+Source data we want to pull lookbacks from:
+- \\spk-netapp2\Hydrology\Studies\SAC-013\Folsom Dam Raise_SOU\99-Working Files\Hamill\Folsom Model\1986D
+
+'''
